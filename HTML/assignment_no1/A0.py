355eba04c45e36f29fe514b8e7f980f10937f773
AkashJ0996 <112629086+AkashJ0996@users.noreply.github.com>
2022-10-08 14:32:38 +0530

    Add files via upload

---
+++ HTML/assignment_no1/A0.py
@@ -0,0 +1,7 @@
+print("lets swap two num ")
+def swap(a,b):
+      print("User Entered A value :%d and B value :%d before swapping "%(a,b))
+      a,b=b,a
+      print("After Swapping a=%d and b=%d"%(a,b))
+print()
+ob=swap(int(input()),int(input()))
